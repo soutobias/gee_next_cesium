ba7f67adc8b040ab483a0d3b21021ea980f7e0e5
tobfer <soutobias@yahoo.com.br>
2024-06-04 09:31:29 +0100

    correct package

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
                     {
                         from: pathBuilder ('node_modules/cesium/Build/Cesium/Workers'),
                         to: '../public/cesium/Workers',
-                        // info: { minimized: true }
+                        info: { minimized: true }
                     }
                 ]
             }),
@@ -21,7 +21,7 @@ const nextConfig = {
                     {
                         from: pathBuilder ('node_modules/cesium/Build/Cesium/ThirdParty'),
                         to: '../public/cesium/ThirdParty',
-                        // info: { minimized: true }
+                        info: { minimized: true }
                     }
                 ]
             }),
@@ -30,7 +30,7 @@ const nextConfig = {
                     {
                         from: pathBuilder ('node_modules/cesium/Build/Cesium/Assets'),
                         to: '../public/cesium/Assets',
-                        // info: { minimized: true }
+                        info: { minimized: true }
                     }
                 ]
             }),
@@ -39,7 +39,7 @@ const nextConfig = {
                     {
                         from: pathBuilder ('node_modules/cesium/Build/Cesium/Widgets'),
                         to: '../public/cesium/Widgets',
-                        // info: { minimized: true }
+                        info: { minimized: true }
                     }
                 ]
             }),
